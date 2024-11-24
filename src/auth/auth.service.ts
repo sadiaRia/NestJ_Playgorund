a2e430a64254ca05ad695cd2de1b076d92959a91
sadia <sadia@sheba.xyz>
2024-11-25 02:20:43 +0600

    functional signup * sign in & add some script to migrate database

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,65 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { User, Bookmark } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signin() {
+  async signin(dto: AuthDto) {
+    //find the user by email
+
+    const user = await this.prisma.user.findUnique({
+      where: { email: dto.email },
+    });
+    if (!user) {
+      throw new ForbiddenException('Credentials exception');
+    }
+    //if user does not exist throw exception
+
+    const pwMatches = await argon.verify(user.hash, dto.password);
+
+    //compare passwords
+
+    if (!pwMatches) {
+      throw new ForbiddenException('Credentials Incorrect');
+    }
+    //if password incorrect throe exception
+    delete user.hash;
+    //send back user
+    return user;
+
     return { msg: 'signin' };
   }
 
-  signup() {
-    return { msg: 'signup' };
+  async signup(dto: AuthDto) {
+    //generate the password  hash
+    const hash = await argon.hash(dto.password);
+
+    //save the user in db
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+        select: {
+          id: true,
+          email: true,
+          createdAt: true,
+        },
+      });
+
+      //return the saved user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+      throw error;
+    }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // take only those which is only entioned in dto
+    }),
+  );
   await app.listen(process.env.PORT ?? 9001);
 }
 bootstrap();
